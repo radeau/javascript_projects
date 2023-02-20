e30f1f930786d79f4a2a53dab213725adb4cdeec
kevinradp <kevinradp@gmail.com>
2023-02-20 11:12:25 +0400

    Update previous code remove the array variable send get request using fetch once there's data display it using dom functions

--- random-quote-generator-using-api/script.js
+++ random-quote-generator-using-api/script.js
@@ -1,60 +1,34 @@
-const quotes = [
-    {
-      text: "The best way to predict the future is to invent it.",
-      author: "Alan Kay"
-    },
-    {
-        text: "Innovation distinguishes between a leader and a follower.",
-      author: "Steve Jobs"
-    },
-    {
-        text: "The only way to do great work is to love what you do.",
-      author: "Steve Jobs"
-    },
-    {
-        text: "You miss 100% of the shots you don't take.",
-      author: "Wayne Gretzky"
-    },
-    {
-        text: "The only thing necessary for the triumph of evil is for good men to do nothing.",
-      author: "Edmund Burke"
-    },
-    {
-        text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-        author: "Nelson Mandela"
-      },
-      {
-        text: "The way to get started is to quit talking and begin doing.",
-        author: "Walt Disney"
-      },
-      {
-        text: "If life were predictable it would cease to be life, and be without flavor.",
-        author: "Eleanor Roosevelt"
-      },
-      {
-        text: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
-        author: "Oprah Winfrey"
-      },
-      {
-        text: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
-        author: "James Cameron"
-      }
-  ];
-
-  function generateQuote() {
-    // Get a random quote object from the quotes array
-    const quote = quotes[Math.floor(Math.random() * quotes.length)];
-  
-    // Display the quote on the web page
-    const quoteTextElement = document.getElementById("quote-text");
-    const quoteAuthorElement = document.getElementById("quote-author");
-    quoteTextElement.textContent = `"${quote.text}"`;
-    quoteAuthorElement.textContent = `- ${quote.author}`;
+function generateQuote() {
+    //Send a GET request to quotable API using fetch
+    fetch("https://api.quotable.io/random")
+      .then(response => {
+        //if the response is not ok throw an error
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        //otherwise, parse the response as JSON
+        return response.json();
+      })
+      //once we have the data, display the quote text and author
+      .then(data => {
+        const quote = data.content;
+        const author = data.author;
+        document.getElementById("quote-text").innerHTML = `"${quote}"`;
+        document.getElementById("quote-author").innerHTML = `- ${author}`;
+      })
+      //if there is an error, log it to the console
+      .catch(error => {
+        console.error("There was a problem fetching the quote:", error);
+      });
   }
-  
-  const newQuoteButton = document.getElementById("new-quote-button");
-  newQuoteButton.addEventListener("click", generateQuote);
-  
-  // Display a random quote on page load
-  generateQuote();
-  
+
+  //Call the generateQuote function when the DOM content has loaded
+  document.addEventListener("DOMContentLoaded", function(event) {
+    generateQuote();
+  });
+
+  //Add an event listener to the "New Quote" button
+  document.getElementById("new-quote-button").addEventListener("click", function(event) {
+    //Call the generateQuote function when the button is clicked 
+    generateQuote();
+  });
